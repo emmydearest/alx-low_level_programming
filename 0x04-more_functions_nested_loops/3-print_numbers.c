1202b3234a0dba8ca75e8bd7628af9c27256cc6a
emmydearest <emediongukpong4@gmail.com>
2023-09-15 16:22:03 -0700

    2 main

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: return void
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
